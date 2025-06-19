fcae2623a36757f7c0f3989baac2b2b0b713f89f
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2025-06-19 10:37:53 +0700

    refactor: improve formatting and indentation in routes configuration

--- app/routes.ts
+++ app/routes.ts
@@ -6,8 +6,11 @@ export default [
 
     layout("components/layouts/default.tsx", [
         layout("components/layouts/criterions.tsx", [
-            layout("components/layouts/dashboard.tsx", [index("pages/dashboard.tsx")]),
-            route('criterions', "pages/criterions.tsx")]),
+            layout("components/layouts/dashboard.tsx", [
+                index("pages/dashboard.tsx")
+            ]),
+            route('criterions', "pages/criterions.tsx")
+        ]),
         route('scales', "pages/scales.tsx"),
     ])
 
